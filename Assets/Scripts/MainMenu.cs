e32dd8beada2949efccf624b91025f7f3951996d
Ulysses-Alv <ulises.j.alvarenga@gmail.com>
2023-09-24 15:32:01 -0300

    Added blackFade; added controlls splash

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,11 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Animator blackFade;
+    [SerializeField] private SpriteRenderer controles;
+
+    private int value = 0;
+    bool IsAlreadyChangingScene;
     void Update()
     {
-        if (Input.anyKeyDown)
+        print(value);
+
+        if (Input.anyKeyDown && !IsAlreadyChangingScene)
         {
-            SceneManager.LoadScene(1);
+            value++;
+            switch (value)
+            {
+                case 0:
+                    ActivateControllerScreen(false);
+                    break;
+
+                case 1:
+                    ActivateControllerScreen(true);
+                    break;
+                case 2:
+                    ActivateControllerScreen(false);
+                    break;
+                default:
+                    blackFade.SetTrigger("StartBlackFade");
+                    Invoke("LoadGame", 1.5f);
+                    IsAlreadyChangingScene = true;
+                    break;
+            }
         }
+    }
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void ActivateControllerScreen(bool setActive)
+    {
+        controles.enabled = setActive;
     }
 }
